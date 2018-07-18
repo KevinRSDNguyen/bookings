759f3f3f7d209e3a4d5d2d1faf1d46bd92f95a25
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-07-18 16:21:52 -0700

    Created fakeDB class

--- server/index.js
+++ server/index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const config = require("./config");
+const FakeDb = require("./fake-db");
 const Rental = require("./models/rental");
 const path = require("path");
 
-mongoose.connect(
-  config.DB_URI,
-  { useNewUrlParser: true }
-);
+mongoose
+  .connect(
+    config.DB_URI,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    const fakeDb = new FakeDb();
+    fakeDb.seedDb();
+  });
 
 const app = express();
 
@@ -21,5 +27,5 @@ app.use(bodyParser.json());
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, function() {
-  console.log("App is running!");
+  console.log("App is running");
 });
